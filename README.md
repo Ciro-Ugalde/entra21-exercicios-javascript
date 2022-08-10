96b39a3db1415f2eb59cb69e5294240d75957d6b
Ciro-Ugalde <ciro.daniel.ugalde@gmail.com>
2022-08-09 23:31:43 -0300

    finalizando exercícios.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # entra21-exercicios-javascript
-Revisão do conteúdo ministrado em aula
+## Revisão do conteúdo ministrado em aula
+
+========================================
+
+Exercícios feitos:
+
+> Variáveis
+
+> Manipulando html com JavaScript
+
+> Eventos entre HTML e JSC
+
+> CRUD
+
+> Consumo de API's
+
--- js/consumindoapi.js
+++ js/consumindoapi.js
@@ -1,7 +1,19 @@
 $.ajax({
-    url: decodeURI("http://makeup-api.herokuapp.com/api/v1/products.json" + $(#param).val()),
+    url: decodeURI("https://makeup-api.herokuapp.com/api/v1/products.json"),
     type: "get",
     success: (retorno) => {
         console.log(retorno);
+        retorno.forEach((item) => {
+            $("table").append(
+                $("<tr>").append(
+                    $("<img>", { src: item.image_link, width: "50px" })
+                ),
+                $("<td>", { text: item.name }),
+                $("<td>", { text: item.description })
+            )
+        });
+    },
+    error: (motivo) => {
+        console.log(motivo);
     }
 })
